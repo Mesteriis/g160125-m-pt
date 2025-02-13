5a2de6a35c8b1258133f9714a89f292bdb314ac8
Daniel Dagner <dagnerdaniel1@gmail.com>
2025-02-13 21:18:06 +0100

    add 00 intro and 01 - l 1

--- 01 - Core python/Урок 14. Работа с файлами/exercise_1.py
+++ 01 - Core python/Урок 14. Работа с файлами/exercise_1.py
@@ -6,15 +6,35 @@
 # 3. Прочитайте первые 10 символов файла и выведите их на экран.
 # 4. Прочитайте одну строку из файла и выведите ее на экран.
 # 5. Прочитайте все строки файла и выведите их на экран.
-
-
+# file_d = open("./text_files/data.txt")
+# content = file_d.read()
+# file_d.seek(0)
+# partial_content = file_d.read(10)
+# file_d.seek(0)
+# one_line = file_d.readline()
+# file_d.seek(0)
+# all_lines = file_d.readlines()
+# print(content)
+# print(partial_content)
+# print(one_line)
+# print(all_lines)
+# file_d.close()
 # Задание 2: Запись данных в файл
 # 1. Откройте (создайте) файл `output.txt` для записи.
 # 2. Запишите в файл строку "Hello, World!".
 # 3. Запишите в файл список строк: ["This is line 1\n", "This is line 2\n"].
 # 4. Закройте файл.
 # 5. Откройте файл `output.txt` для чтения и выведите его содержимое на экран.
-
+# file_o = open("./text_files/output.txt", "w")
+# file_o.write("Hello , World!\n")
+# lines_to_append = ["This is line 1\n", "This is line 2\n"]
+# file_o.writelines(lines_to_append)
+# file_o.close()
+#
+# file_o = open("./text_files/output.txt", "r")
+# all_file = file_o.read()
+# print(all_file)
+# file_o.close()
 
 # Задание 3: Добавление данных в файл
 # 1. Откройте (создайте) файл `log.txt` для добавления данных.
@@ -22,7 +42,16 @@
 # 3. Добавьте список строк ["Log entry 1\n", "Log entry 2\n"] в конец файла.
 # 4. Закройте файл.
 # 5. Откройте файл `log.txt` для чтения и выведите его содержимое на экран.
-
+# file_l = open("./text_files/log.txt", "w")
+# file_l.write("New log entry\n")
+# lines_to_append = ["Log entry 1\n", "Log entry 2\n"]
+# file_l.writelines(lines_to_append)
+# file_l.close()
+#
+# file_l = open("./text_files/log.txt", "r")
+# printend_file = file_l.read()
+# print(printend_file)
+# file_l.close()
 
 # Задание 4: Работа с указателем
 # 1. Откройте (создайте) файл `pointer_example.txt` для чтения и записи.
@@ -31,4 +60,17 @@
 # 4. Переместите указатель на позицию 7 и прочитайте следующие 5 символов.
 # 5. Переместите указатель в конец файла и добавьте строку "End of file\n".
 # 6. Переместите указатель в начало файла и прочитайте весь файл.
+# file_p = open("./text_files/pointer_example.txt", 'w')
+# file_p.close()
+# file_p = open("./text_files/pointer_example.txt", 'r+')
+# file_p.write("Python File Handling\n")
+# file_p.seek(0)
+# file_p.read()
+# file_p.seek(7)
+# file_p.read(5)
+# file_p.seek(0, 2)
+# file_p.write("End of file\n")
+# file_p.seek(0)
+# all_file = file_p.read()
+# print(all_file)
 
--- 01 - Core python/Урок 15. Обработка ошибок/exercise_2.py
+++ 01 - Core python/Урок 15. Обработка ошибок/exercise_2.py
@@ -1,12 +1,12 @@
-# Тема: Расространение исключения. Возбуждение исключения.
-
-# Задача 1. Допишите код ниже.
+# # Тема: Расространение исключения. Возбуждение исключения.
 #
+# # Задача 1. Допишите код ниже.
+# #
 # import math
 #
 # def calculate_square_root(number):
-#     # Добавьте проверку на отрицательное число и возбуждение исключения
-#
+#     if number < 0:
+#         raise ValueError("Число должно быть положительным")
 #     return math.sqrt(number)
 #
 # def safe_calculate_square_root(number):
@@ -16,15 +16,14 @@
 #     except ValueError as e:
 #         print(f"Ошибка: {e}")
 #
-# # Тесты функции
 # safe_calculate_square_root(25)  # Ожидаемый результат: Квадратный корень из 25 равен 5.0
 # safe_calculate_square_root(-9)  # Ожидаемый результат: Ошибка: Число должно быть положительным
-
-
-# Задача 2. Допишите код ниже.
-# def convert_to_number(string):
-#     # Добавьте проверку на некорректное значение и возбуждение исключения
 #
+#
+# # Задача 2. Допишите код ниже.
+# def convert_to_number(string):
+#     if not string.isdigit():
+#         raise ValueError("Введенное значение не является числом")
 #     return int(string)
 #
 # def safe_convert_to_number(string):
@@ -34,6 +33,5 @@
 #     except ValueError as e:
 #         print(f"Ошибка: {e}")
 #
-# # Тесты функции
 # safe_convert_to_number("123")  # Ожидаемый результат: Конвертированное число: 123
 # safe_convert_to_number("abc")  # Ожидаемый результат: Ошибка: Введенное значение не является числом
