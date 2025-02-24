dd1fbdbdace5a96ffd1d9c39ec0005cb141ac536
Maxx <m.petrykin@gmx.de>
2025-02-24 22:36:49 +0100

    homework for 02-L1 done.

--- Модуль 3. Advanced Python/Урок 1. Введение в ООП/src/Exercises/01 - Classes.py
+++ Модуль 3. Advanced Python/Урок 1. Введение в ООП/src/Exercises/01 - Classes.py
@@ -4,69 +4,48 @@
 # Класс `Student` должен иметь дополнительный атрибут `student_id`, а класс `Teacher` — атрибут `subject`.
 # Реализуйте методы для вывода информации о каждом объекте.
 
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#
+# class Student(Person):
+#     def __init__(self, name, age, student_id):
+#         super().__init__(name, age)
+#         self.student_id = student_id
+#         print(f"Class Person __init__(student_id={self.student_id})")
+#
+# class Teacher(Person):
+#         def __init__(self, name, age, subject):
+#             super().__init__(name, age)
+#             self.subject = subject
+#             print(f"Class Person __init__(subject = {self.subject})")
+
+
 # Ex 2
 # Создайте класс `Vehicle` с атрибутами `make` и `model`.
 # Создайте производный класс `Car` с дополнительным атрибутом `num_doors` (количество дверей) и
 # производный класс `Motorcycle` с атрибутом `has_sidecar` (имеет ли мотоцикл коляску).
 # Реализуйте метод, который выводит полное описание транспортного средства.
 
-class Person:
-    """
-    Base class Person
-    attributes:
-        name: string
-        age: int
-    """
-    pass  # you code here
+# class Vehicle:
+#     def __init__(self, make, model):
+#         self.make = make
+#         self.model = model
+#
+#
+# class Car(Vehicle):
+#    def __init__(self, make, model, num_doors):
+#        super().__init__(make, model)
+#        self.num_doors = num_doors
+#        print(f"Class vehicle __init(num_doors = {self.num_doors}")
+#
+#
+# class Motorcycle(Vehicle):
+#        def __init__(self, make, model, has_sidecar):
+#         super().__init__(make, model)
+#         self.has_sidecar = has_sidecar
+#         print(f"Class vehicle __init(has_sidecar = {self.has_sidecar}")
+#
 
-
-class Student(Person):
-    """
-    Class Student
-    attributes: student_id: int
-    """
-    pass  # you code here
-
-
-class Teacher(Person):
-    """
-    Class Teacher
-    attributes: subject: string
-    """
-    pass  # you code here
-
-
-class Vehicle:
-    """
-    Class Vehicle
-    attributes:
-        make: string
-        model: string
-    """
-    pass  # you code here
-
-
-class Car(Vehicle):
-    """
-    Class Car
-    attributes:
-        num_doors: int
-    """
-    pass  # you code here
-
-
-class Motorcycle(Vehicle):
-    """
-    Class Motorcycle
-    attributes:
-        has_sidecar: bool
-    """
-    pass  # you code here
-
-
-def main():
-    pass  # you code here
-
-
-if __name__ == '__main__':
-    main()
--- Модуль 3. Advanced Python/Урок 1. Введение в ООП/src/Exercises/03 - Public_private_attributes.py
+++ Модуль 3. Advanced Python/Урок 1. Введение в ООП/src/Exercises/03 - Public_private_attributes.py
@@ -2,9 +2,67 @@
 # Создайте класс `BankAccount` с публичным атрибутом `owner` и приватным атрибутом `__balance`.
 # Реализуйте методы для внесения депозита и снятия денег, обеспечивая корректность операций
 # (например, нельзя снять больше, чем есть на балансе).
+# class BankAccount:
+#     total_balance = 0
 #
+#     def __init__(self, owner, balance):
+#         self.owner = owner
+#         self.__balance = balance
+#         BankAccount.total_balance += balance
+#
+#     def get_bal(self):
+#         return self.__balance
+#
+#     def add_bal(self, amount):
+#         if amount > 0:
+#             self.__balance += amount
+#             BankAccount.total_balance += amount
+#
+#     def withdraw(self, amount):
+#         if amount > 0:
+#             if self.__balance >= amount:
+#                 self.__balance -= amount
+#                 BankAccount.total_balance -= amount
+#             else:
+#                 print("Not enough money")
+
+# user1 = BankAccount("Max", 100)
+# print(f"Balance Max: {user1.get_bal()}")
+#
+# user1.add_bal(200)
+# print(f"Balance Max: {user1.get_bal()}")
+#
+# user1.withdraw(50)
+# print(f"Current Balance Max: {user1.get_bal()}")
+#
+# user1.withdraw(1000)
+# print(f"Balance change to: {user1.get_bal()}")
+
 # Ex 2
 # Создайте класс `Product` с публичным атрибутом `name` и приватным атрибутом `__price`.
 # Реализуйте методы для получения и изменения цены,
 # добавив проверки на корректность (цена не может быть отрицательной).
 
+# class Product:
+#     t_price = 0
+#
+#     def __init__(self, name, price):
+#         self.name = name
+#         self.__price = price
+#         Product.t_price += price
+#
+#     def get_prods_list(self):
+#         return self.__price
+#
+#     def change_price(self, amount):
+#             if amount > 0:
+#                 self.__price += amount
+#                 Product.t_price += amount
+#             else:
+#                 print("Sum must be positive")
+#
+# product1 = Product("Iphone", 100)
+# print(f"Current product price: {product1.get_prods_list()}")
+#
+# product1.change_price(200)
+# print(f"Product changed to: {product1.get_prods_list()}")
