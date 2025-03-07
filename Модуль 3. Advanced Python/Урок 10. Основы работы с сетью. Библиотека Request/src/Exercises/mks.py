88eab3ff9d0aee78e6ba181f349e7a0688392b65
Aleksei <a.ossadtsi@gmail.com>
2025-03-07 11:59:14 +0100

    please show

--- Модуль 3. Advanced Python/Урок 10. Основы работы с сетью. Библиотека Request/src/Exercises/mks.py
+++ Модуль 3. Advanced Python/Урок 10. Основы работы с сетью. Библиотека Request/src/Exercises/mks.py
@@ -2,3 +2,30 @@
 # вывести результаты в понятном для человека виде
 # сайт с информацией http://api.open-notify.org
 
+import requests
+
+coords_url = "http://api.open-notify.org/iss-now.json"
+response = requests.get(coords_url)
+coords_data = response.json()
+
+latitude = coords_data['iss_position']['latitude']
+longitude = coords_data['iss_position']['longitude']
+
+crew_url = "http://api.open-notify.org/astros.json"
+response = requests.get(crew_url)
+crew_data = response.json()
+
+iss_crew = [astronaut['name'] for astronaut in crew_data['people'] if astronaut['craft'] == 'ISS']
+
+print("Текущие координаты МКС:")
+print(f"  Широта: {latitude}°")
+print(f"  Долгота: {longitude}°")
+print()
+
+print("Состав экипажа на борту МКС:")
+if iss_crew:
+    for i, name in enumerate(iss_crew, start=1):
+        print(f"  {i}. {name}")
+else:
+    print("Не удалось получить данные об экипаже.")
+
